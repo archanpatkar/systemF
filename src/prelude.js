9cba2317fbae1487907bea89fee0bac4228427be
Archan Patkar <pat@archanpatkar.com>
2021-04-04 16:07:39 +0530

    wip

---
+++ src/prelude.js
@@ -0,0 +1,14 @@
+// WIP
+// (\x:@X.X->X. x [@X. X->X] x) (?a. \x:a. x)
+// let Pair = (?t1. ?t2. \x:t1. \y:t2. ?r. \f:t1->t2->r. f x y)
+// (Pair [number] [number] 10 20) [number] (\x:number. \y:number. x)
+// let fst = (?t1. ?t2. \x:@R. (t1->t2->R)->R. x [t1] (\e1:t1. \e2:t2. e1))
+// (fst [number] [number]) ps1
+// let snd = (?t1. ?t2. \x:@R. (t1->t2->R)->R. x [t2] (\e1:t1. \e2:t2. e2))
+// (fst [number] [number]) ps1
+// (Pair [number] [number] 10 20)
+// let id = (?t. \x:t. x)
+// id [int] 10
+// id [int] 10
+// (?a. \x:a->a. x) [int->int]
+// (\x:int->int. x 3) (\x:int. x + 10)
